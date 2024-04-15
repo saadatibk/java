ce4c9823d0a3c193db25787575c7595070a3d5eb
saadatibk <164902473+saadatibk@users.noreply.github.com>
2024-04-15 15:59:12 -0500

    Create sum_array.java

---
+++ sum_array.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int[] my_array = {1,3,4,5,6};
+        int sum = 0;
+
+        for(int i = 0; i < my_array.length; i++){
+            sum += my_array[i];
+        }
+
+    System. out. println(sum);
+    }
+}
