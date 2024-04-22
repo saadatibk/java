e60dcfc94ddd0d623ad9bd779bdeef7b2222cb5e
saadatibk <164902473+saadatibk@users.noreply.github.com>
2024-04-21 23:41:37 -0500

    Update Product_of_Array_Except_Self.java

--- Product_of_Array_Except_Self.java
+++ Product_of_Array_Except_Self.java
@@ -10,22 +10,25 @@ class Solution {
         }
 
         pref_mul[0] = nums[0];
+        
         for (int i = 1; i < n; i++){
             pref_mul[i] = pref_mul[i - 1] * nums[i];
         } 
 
         suf_mul[n - 1] = nums[n - 1];
+        
         for (int i = n - 2; i >= 0 ; i--){
             suf_mul[i] = suf_mul[i + 1] * nums[i];
         }
 
-        int[] answer = new int[n];
+        int[] result = new int[n];
         answer[0] = suf_mul[1];
         answer[n - 1] = pref_mul[n-2];
+        
         for(int i = 1 ; i < n - 1; i++){
             answer[i] = pref_mul[i - 1] * suf_mul[i + 1];
         }
 
-        return answer;
+        return result;
     }
 }
