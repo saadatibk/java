73d5c847c152f129591c0998cdb8fcb4a20e84da
saadatibk <164902473+saadatibk@users.noreply.github.com>
2024-05-08 23:17:23 -0500

    Update fast_exponentiation.java

--- fast_exponentiation.java
+++ fast_exponentiation.java
@@ -6,7 +6,6 @@ class Codechef
 {
     public static void main (String[] args) throws java.lang.Exception
     {
-        // your code goes here
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         while (t > 0) {
